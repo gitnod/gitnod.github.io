73a18dba09f880fbcff78dbb5cd62a5a23b03434
gitnod <>
2019-05-21 21:35:48 -0500

    restore commit

---
+++ indexScript.js
@@ -0,0 +1,10 @@
+var fgoCalcButton = document.getElementById("fgoCalcButton");
+var rediveCalcButton = document.getElementById("rediveCalcButton");
+
+fgoCalcButton.addEventListener("click", function() {
+    window.location = "./fgocalc/";
+});
+
+rediveCalcButton.addEventListener("click", function() {
+    window.location = "./redivecalc/";
+});
